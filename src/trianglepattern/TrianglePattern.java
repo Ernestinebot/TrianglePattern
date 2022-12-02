95808d0e24e9823fa85b4f545e44f624d6c0ec51
Ernestinebot <116901955+Ernestinebot@users.noreply.github.com>
2022-12-02 11:22:40 -0500

    Initial commit

---
+++ src/trianglepattern/TrianglePattern.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package trianglepattern;
+
+/**
+ *
+ * @author ernestine. committing to memory the concepts associated with sout tab and System.out.println
+ */
+public class TrianglePattern {   //File must be saved as TrianglePattern.java
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) { 
+         System.out.println("* * * * *"); //Note System.out.println ("aString"); The semi-colon ends the programming statement
+         System.out.println(" *     *");
+         System.out.println("  *   *");
+         System.out.println("   * *");
+         System.out.println("    *");
+    }
+    
+}
